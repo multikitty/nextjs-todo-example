3366708f8ab506e8e07d92e994ecc4001bc1dd32
multikitty <multikitty422@gmail.com>
2022-01-12 00:17:14 +0330

    feat: clear all todo

--- components/Todos.jsx
+++ components/Todos.jsx
@@ -1,30 +1,38 @@
 import React, { useState } from "react";
 import Todolist from "./TodoList";
+import { toast, ToastContainer } from "react-toastify";
 
 const Todos = () => {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
 
   const handleAddTodo = () => {
-    setTodos([...todos, { id: Math.floor(Math.random() * 1000), text,completed:false }]);
-    setTimeout(() =>{
-        setText('')
-    },500)
+    setTodos([
+      ...todos,
+      { id: Math.floor(Math.random() * 1000), text, completed: false },
+    ]);
+    setTimeout(() => {
+      setText("");
+    }, 500);
   };
 
-  const handleDeleteTodo = (id) =>{
-    const allTodos = [...todos]
-    const filteredTodo = allTodos.filter(todo => todo.id !==id)
-    setTodos(filteredTodo)
-  }
+  const handleDeleteTodo = (id) => {
+    const allTodos = [...todos];
+    const filteredTodo = allTodos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodo);
+  };
 
-  const handleCompleteTodo = (id) =>{
-    const allTodos = [...todos]
-    const todo = allTodos.findIndex(todo => todo.id ===id) 
-    let newTodo = allTodos[todo]
+  const handleCompleteTodo = (id) => {
+    const allTodos = [...todos];
+    const todo = allTodos.findIndex((todo) => todo.id === id);
+    let newTodo = allTodos[todo];
     newTodo.completed = true;
     allTodos[todo] = newTodo;
-    setTodos(allTodos)
+    setTodos(allTodos);
+  };
+
+  const handleClearAllTodo = () =>{
+    setTodos([])
   }
 
   return (
@@ -35,10 +43,14 @@ const Todos = () => {
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={handleAddTodo}>
-          Add Todo
-      </button>
-      <Todolist todos={todos} deleteTodo={handleDeleteTodo} completeTodo={handleCompleteTodo} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearAllTodo} className="pl-2 bg-rose-600">Clear all</button>
+      <Todolist
+        todos={todos}
+        deleteTodo={handleDeleteTodo}
+        completeTodo={handleCompleteTodo}
+      />
+      <ToastContainer />
     </div>
   );
 };
